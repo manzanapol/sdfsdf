b00c387792d2b7e4c24531348e3cda9edb4bf12a
manzanapol <43615936+manzanapol@users.noreply.github.com>
2018-09-26 11:49:32 -0500

    archivos
    todos

---
+++ jquery/index.js
@@ -0,0 +1,55 @@
+$( document ).ready(function() {
+	$(window).on('click', function() {
+		$('.menu-options').removeClass('active');
+	});
+   $('.header-wrapper .option').on('click', dropdownMenu); 
+   $('#slides').superslides();
+
+
+});
+
+var dropdownMenu = function(event) {
+	dropdownElement = $(this).next();
+	$('.menu-options').removeClass('active');
+	dropdownElement.addClass('active');
+	event.stopPropagation();
+}
+
+$(".js-carousel").each(function(){
+	var $carousel = $(this),
+		$carouselContainer = $carousel.find(".js-carousel-container"),
+		$carouselList = $carousel.find(".js-carousel-list"),
+		$carouselItem = $carousel.find(".js-carousel-item"),
+		$carouselButton = $carousel.find(".js-carousel-button"),
+		setItemWidth = function(){
+			$carouselList.removeAttr("style");
+			var curWidth = $($carouselItem[0]).outerWidth() * $carouselItem.length;
+			$carouselList.css("width", curWidth);
+		},
+		slide = function(){
+			var $button = $(this),
+				dir = $button.data("dir"),
+				curPos = parseInt($carouselList.css("left")) || 0,
+				moveto = 0,
+				containerWidth = $carouselContainer.outerWidth(),
+				listWidth = $carouselList.outerWidth(),
+				before = (curPos + containerWidth),
+				after = listWidth + (curPos - containerWidth);
+			if(dir=="next"){
+				moveto = (after < containerWidth) ? curPos - after : curPos - containerWidth;
+			} else {
+				moveto = (before >= 0) ? 0 : curPos + containerWidth;
+			}
+			
+			
+			$carouselList.animate({
+				left: moveto
+			});
+		};
+	$(window).resize(function(){
+		setItemWidth();
+	});
+	setItemWidth();
+	
+	$carouselButton.on("click", slide);
+});
